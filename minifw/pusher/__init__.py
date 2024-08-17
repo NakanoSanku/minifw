4248f268095cdbc4ef261400873ca9ff05efef0b
NakanoSanku <alexkaykt@outlook.com>
2024-08-18 07:14:36 +0800

    fix(pusher): 防止初始化时推送模块导入失败
    解决了一个在pusher子模块导入失败的问题，该问题源于[__init__.py](file://minifw/pusher/__init__.py)文件中缺少导入语句。此修复确保了Pusher、PusherOptions、PushPlus、PushPlusOptions、ServerGirl、ServerGirlOptions、WxPusher和WxPusherOptions类在模块初始化时正确导入。

---
+++ minifw/pusher/__init__.py
@@ -0,0 +1,4 @@
+from .pusher import Pusher, PusherOptions
+from .pushplus import PushPlus, PushPlusOptions
+from .servergirl import ServerGirlOptions, ServerGirl
+from .wxpusher import WxPusher, WxPusherOptions
