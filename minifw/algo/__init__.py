6c6e5905af2e16e75a8c05f8f2fffe4a3ae2f886
NakanoSanku <alexkaykt@outlook.com>
2024-08-19 06:28:35 +0800

    feat(algo): 新增区域随机点生成器
    新增各种生成器到`minifw.algo`模块，包括区域随机点生成器、中心点生成器和正态分布点生成器。这些生成器旨在满足不同场景下点坐标生成的需求，提升算法模块的灵活性和多样性。

---
+++ minifw/algo/__init__.py
@@ -0,0 +1,12 @@
+from minifw.algo.generate import (
+    Generator,
+    # 点生成器
+    PointGenerator,
+    # 偏移点生成器
+    OffsetPointGenerator,
+    NoneOffsetPointGenerator,
+    # 区域随机点生成器
+    RegionPointGenerator,
+    CenterPointGenerator,
+    NormalDistributionPointGenerator
+)
