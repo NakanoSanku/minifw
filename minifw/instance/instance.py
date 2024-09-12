406152a4ad1598007d8e6653733f4be4902c7867
NakanoSanku <alexkaykt@outlook.com>
2024-09-12 20:32:11 +0800

    重构`ScriptInstance`类的实现，优化代码结构和可读性。
    - 移除`ScriptInstanceConfig`数据类，直接将配置参数传递给`ScriptInstance`构造函数。
    - 在`ScriptInstance`初始化中直接管理`debug`、`keyboard_method`、`touch_method`和`screencap_method`等配置项。
    - 更新`find_and_click`方法，移除未使用的`algorithm`参数。
    - 添加`sleep`静态方法用于精确控制暂停。
    - 调整`find`方法，移除调试信息中未必要的注释代码块。
    - 精简代码，移除未使用的导入和方法，如`draw_result_in_screen`。
    
    这些重构操作旨在简化实例化过程，优化方法实现，并通过清晰的代码结构提升可维护性和可扩展性。

--- minifw/instance/instance.py
+++ minifw/instance/instance.py
@@ -1,17 +1,12 @@
 import functools
 import time
-from dataclasses import dataclass
-from threading import Thread
-from time import sleep
-from uuid import uuid4
 
 import cv2
 from loguru import logger
 
-from minifw.cv import bytes2mat, rectangle, point, destroy_window
+from minifw.cv import bytes2mat
 from minifw.keyboard import Keyboard
 from minifw.matcher import MatchResult, Template
-from minifw.matcher.result import RectMatchResult, PointMatchResult
 from minifw.screencap import ScreenCap
 from minifw.touch import Touch
 
@@ -34,118 +29,85 @@ def performance_test(func):
     return wrapper
 
 
-@dataclass
-class ScriptInstanceConfig:
-    screencap_method: ScreenCap = None
-    touch_method: Touch = None
-    keyboard_method: Keyboard = None
-    debug: bool = False
-    draw_color: int | str = "#DC143C"
-    draw_size: int = 1
-    winname: str = str(uuid4())
-
-
 class ScriptInstance(ScreenCap, Touch, Keyboard):
-    def __init__(self, config: ScriptInstanceConfig):
-        self.config = config
-        self.imshow_flag = True
-        self.imshow_thread = None
+    def __init__(self, screencap_method: ScreenCap = None,touch_method: Touch = None, keyboard_method: Keyboard = None,debug: bool = False):
+        self.debug = debug
+        self.keyboard_method = keyboard_method
+        self.touch_method = touch_method
+        self.screencap_method = screencap_method
 
     def screencap_raw(self) -> bytes:
-        if self.config.screencap_method is None:
+        if self.screencap_method is None:
             raise Exception("未指定截图方式")
-        return self.config.screencap_method.screencap_raw()
+        return self.screencap_method.screencap_raw()
 
     @performance_test
     def screencap(self) -> cv2.Mat:
         return bytes2mat(self.screencap_raw())
 
     @performance_test
-    def click(self, x: int, y: int, duration: int = 100):
-        if self.config.touch_method is None:
+    def click(self, x: int, y: int, duration: int = 150):
+        if self.touch_method is None:
             raise Exception("未指定触摸方式")
         self.debug_log(f"Click at point({x},{y}) in {duration}ms")
-        return self.config.touch_method.click(x, y, duration)
+        return self.touch_method.click(x, y, duration)
 
     @performance_test
-    def swipe(self, points: list, duration: int = 300):
-        if self.config.touch_method is None:
+    def swipe(self, points: list, duration: int = 500):
+        if self.touch_method is None:
             raise Exception("未指定触摸方式")
         self.debug_log(f"Swipe from {points[0]} to {points[-1]} in {duration}ms")
-        return self.config.touch_method.swipe(points, duration)
+        return self.touch_method.swipe(points, duration)
 
     @performance_test
     def find(self, template: Template) -> MatchResult:
         screen = self.screencap()
         result = template.match(screen)
         result.set_controller(self)
-        if self.config.debug:
+        if self.debug:
             logger.debug(f"Find {template} in {result.get()}")
-            #TODO: DEBUG功能会导致程序卡顿并且模板匹配结果不准确（原因未知）暂时弃用
-            # self.imshow_flag = False
-            # if self.imshow_thread is not None:
-            #     self.imshow_thread.join()
-            # self.imshow_flag = True
-            # self.imshow_thread = Thread(target=self.draw_result_in_screen, args=(screen, result))
-            # self.imshow_thread.start()
-
         return result
 
     @performance_test
     def key_down(self, key: str) -> None:
-        if self.config.keyboard_method is None:
+        if self.keyboard_method is None:
             raise Exception("未指定键盘输入方式")
         self.debug_log(f"Key down {key}")
-        self.config.keyboard_method.key_down(key)
+        self.keyboard_method.key_down(key)
 
     @performance_test
     def key_up(self, key: str) -> None:
-        if self.config.keyboard_method is None:
+        if self.keyboard_method is None:
             raise Exception("未指定键盘输入方式")
         self.debug_log(f"Key up {key}")
-        self.config.keyboard_method.key_up(key)
+        self.keyboard_method.key_up(key)
 
     @performance_test
-    def find_and_click(self, template: Template, duration: int = 100, algorithm=None) -> MatchResult:
+    def find_and_click(self, template: Template, duration: int = 150, algorithm=None) -> MatchResult:
         result = self.find(template)
         result.click(self, duration, algorithm) if algorithm else result.click(self, duration)
         return result
 
-    def draw_result_in_screen(self, screen, result: MatchResult):
-        # 如果result是RectMatchResult就进行方框绘制
-        if isinstance(result, RectMatchResult):
-            # 显示绘制方框的截图
-            screen = rectangle(screen, result.get(), self.config.draw_color, self.config.draw_size)
-        # 如果result是PointMatchResult就进行方框绘制
-        elif isinstance(result, PointMatchResult):
-            # 显示绘制点的截图
-            screen = point(screen, result.get(), self.config.draw_size, self.config.draw_color)
-        cv2.imshow(self.config.winname, screen)
-        while self.imshow_flag:
-            # 检查窗口是否关闭
-            if cv2.getWindowProperty(self.config.winname, cv2.WND_PROP_VISIBLE) < 1:
-                break
-            # 检查是否有按键被按下
-            if cv2.waitKey(1) != -1:
-                break
-        destroy_window(self.config.winname)
-
     def debug_log(self, msg: str):
-        if self.config.debug:
+        if self.debug:
             logger.debug(msg)
+
+    @staticmethod
+    def sleep(duration: int):
+        time.sleep(duration/1000)
 
 
 if __name__ == '__main__':
     from minifw.screencap import ADBCap
     from minifw.touch import ADBTouch
-    from minifw.matcher.image import ImageTemplate
+    from minifw.matcher import  ImageTemplate
+
 
     screencap_method = ADBCap(serial="127.0.0.1:16384")
     touch_method = ADBTouch(serial="127.0.0.1:16384")
-    config = ScriptInstanceConfig(screencap_method=screencap_method, touch_method=touch_method, debug=True)
-    instance = ScriptInstance(config)
+    instance = ScriptInstance(screencap_method=screencap_method, touch_method=touch_method, debug=True)
     t = ImageTemplate(r"C:\Users\KateT\Desktop\QQ截图20240819103933.png")
     instance.find(t).click(instance, duration=150)
-    sleep(3)
+    instance.sleep(3)
     instance.find(t)
     pass
